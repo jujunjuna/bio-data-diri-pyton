e232835befb301ef3dd817cdfe4b627272dce39d
jujunjuna <jujunn.0698@gmail.com>
2021-11-02 21:25:45 +0700

    memasukan file program

---
+++ program_biodata-diri.py
@@ -0,0 +1,16 @@
+nama_lengkap=input(str('masukan nama lengkap: '))
+nama_panggilan=input(str('masukan nama panggilna: '))
+alamat=input(str('masukan alamat: '))
+tempat_lahir=input(str('masukan tempat lahir: '))
+tanggal_lahir=int(input('masukan tgl lahir: '))
+npm=str(input('masukan npm: '))
+tlp=int(input('masukan nmr tlp: '))
+umur=int(input('masukan umur: '))
+
+print('Assalamualaikum')
+
+print('Let me introduce my self. My name is',nama_lengkap,',but you can call me',nama_panggilan,'.')
+print('My npm is',npm,'. I was born in',tempat_lahir,'and im',umur,'yeard old.')
+print('I am very glad if you want to invite my house in',alamat,'. dont forget to call me before with the number',tlp,'.')
+
+print('Thank you.')
